217e382a4e813a9b418e05bb3ce2eb178268f00d
Joel McQuaid <jamcquaid7@gmail.com>
2025-06-03 14:00:28 -0400

    Modifying plane slicer for 3-point definitions.

--- src/analysis/plane_slice.h
+++ src/analysis/plane_slice.h
@@ -9,10 +9,10 @@ namespace pptb::analysis
 		using value_type = rtype;
 		
 		// Member variables
-		std::array<value_type, 3> nvec; // Plane normal
-		std::array<value_type, 3> tvec; // Plane tangential vector 1
-		std::array<value_type, 3> mvec; // plane tangential vector 2
-		std::array<value_type, 3> pt;
+		spade::ctrs::array<value_type, 3> nvec; // Plane normal
+		spade::ctrs::array<value_type, 3> tvec; // Plane tangential vector 1
+		spade::ctrs::array<value_type, 3> mvec; // plane tangential vector 2
+		spade::ctrs::array<value_type, 3> pt;
 	};
 
 	template<std::floating_point rtype>
@@ -21,13 +21,21 @@ namespace pptb::analysis
 		rtype xyz[3];
 		rtype data[100];
 	};
+
+	template <std::floating_point rtype>
+	struct truncate_t
+	{
+		int type=-1;
+		int dir=0;
+		rtype val=0.0;
+	};
 	
 	template <std::floating_point rtype>
 	struct plane_slice_t
 	{
 		using value_type  = rtype;
 		using surf_geom_t = geom::surf_geom_t<value_type>;
-		using vec_t = std::array<value_type, 3>;
+		using vec_t = spade::ctrs::array<value_type, 3>;
 		using container_t = std::vector<vec_t>;
 		
 		// Member variables
@@ -36,11 +44,13 @@ namespace pptb::analysis
 		std::vector<std::size_t> slice_plane; // Slice plane
 		std::vector<value_type> slice_pos; // Slice position in plane normal direction
 		std::vector<std::string> slice_file; // Import slice plane points from file
-		std::vector<container_t> slice_file_points; // Imported point coordinates from slice file
+		std::vector<spade::ctrs::array<vec_t, 3>> slice_file_points; // Imported point coordinates from slice file
 		std::vector<plane_t<value_type>> planes; // Slice plane basis
+		std::vector<truncate_t<value_type>> truncate; // Truncater
 		bool normalize_coord = false;
 		int normalize_dir    = 0;
 		int nslice;
+		value_type scaling = 1.0;
 
 		// Get coordinate on plane
 		inline vec_t get_plane_pt(const auto& iplane)
@@ -60,11 +70,21 @@ namespace pptb::analysis
 				print("      mvec = ",planes[n].mvec[0], planes[n].mvec[1], planes[n].mvec[2]);
 				print("      pt   = ",planes[n].pt[0], planes[n].pt[1], planes[n].pt[2]);
 			}
+			else if (slice_plane[n] == 3)
+			{
+				print("   Slice = ",n," name = ",slice_name[n], " plane,ID = ",slice_plane[n],slice_plane_in[n]);
+				print("      file = ",slice_file[n]);
+				print("      nvec = ",planes[n].nvec[0], planes[n].nvec[1], planes[n].nvec[2]);
+				print("      tvec = ",planes[n].tvec[0], planes[n].tvec[1], planes[n].tvec[2]);
+				print("      mvec = ",planes[n].mvec[0], planes[n].mvec[1], planes[n].mvec[2]);
+				print("      pt   = ",planes[n].pt[0], planes[n].pt[1], planes[n].pt[2]);
+			}
 			else
 			{
 				print("   Slice = ",n," name = ",slice_name[n], " plane,ID = ",slice_plane[n],slice_plane_in[n]);
-				print("      file, npts = ",slice_file[n],slice_file_points[n].size());
+				print("      No. of probes = ",slice_data.size());
 			}
+			if (truncate[n].type>-1) print("      Truncating. type, dir, val = ", truncate[n].type, truncate[n].dir, truncate[n].val);
 		}
 
 		void import_slice_points(const int& n)
@@ -81,28 +101,78 @@ namespace pptb::analysis
 				++count;
 			}
 
-			// Resize container
-			slice_points.resize(count);
-
 			// Close file
 			fh.fh.close();
 
 			// Reopen file
 			spade::utils::ascii_file_t file(slice_file[n]);
-
+			
 			// Import data
 			for (int i = 0; i<slice_points.size(); ++i)
 			{
 				auto& ptArr = slice_points[i];
 				file.next_line();
 				file.parse(ptArr[0], ptArr[1], ptArr[2]);
+
+				ptArr *= scaling;
+			}
+
+			// tvec
+			auto tvec = slice_points[1] - slice_points[0];
+			tvec     /= spade::ctrs::array_norm(tvec);
+			planes[n].tvec = tvec;
+
+			// mvec
+			auto mvec = slice_points[2] - slice_points[1];
+			mvec     /= spade::ctrs::array_norm(mvec);
+			planes[n].mvec = mvec;
+
+			// nvec
+			planes[n].nvec = spade::ctrs::cross_prod(planes[n].tvec, planes[n].mvec);
+
+			// Set plane point
+			planes[n].pt   = slice_points[0];
+		}
+
+		void import_probe_points(const int& n)
+		{
+			auto& slice = slice_data[n];
+
+			// First check for number of points in file
+			int count = 0;
+			spade::utils::ascii_file_t fh(slice_file[n]);
+			while (!fh.eof())
+			{
+				fh.next_line();
+				if (fh.eof()) break;
+				++count;
+			}
+
+			// Set cross-section memory
+			slice.nodes.resize(count);
+
+			// Close file
+			fh.fh.close();
+
+			// Reopen file
+			spade::utils::ascii_file_t file(slice_file[n]);
+			
+			// Import data
+			for (int i = 0; i<slice.nodes.size(); ++i)
+			{
+				auto& ptArr = slice.nodes[i];
+				file.next_line();
+				file.parse(ptArr[0], ptArr[1], ptArr[2]);
+				const std::string delimiter = ",";
+				std::vector<std::string> result = utils::splitString(file.p_line, delimiter);
+				for (int j = 0; j<result.size(); ++j) ptArr[j] = std::stod(result[j]);
 			}
 		}
 		
 		// Constructor --> slice select based on Cartesian planes
 	    plane_slice_t(const auto& slice_name_in, const auto& slice_plane_in, const auto& slice_pos_in,
-					  const auto& slice_file_in, const bool& norm_coord_in = false, const int& norm_dir_in = 0)
-		: slice_pos{slice_pos_in}, slice_file{slice_file_in}, normalize_coord{norm_coord_in}, normalize_dir{norm_dir_in}
+					  const auto& slice_file_in, const auto& truncate_in, const auto& scaling_in, const bool& norm_coord_in = false, const int& norm_dir_in = 0)
+		: slice_pos{slice_pos_in}, slice_file{slice_file_in}, normalize_coord{norm_coord_in}, normalize_dir{norm_dir_in}, scaling{scaling_in}
 		{
 			print("Initializing slice operator...");
 
@@ -116,6 +186,7 @@ namespace pptb::analysis
 			slice_pos.resize(nslice);
 			slice_file_points.resize(nslice);
 			planes.resize(nslice);
+			truncate.resize(nslice);
 			
 			for (int n = 0; n<nslice; n++)
 			{
@@ -141,11 +212,17 @@ namespace pptb::analysis
 					planes[n].tvec = {0.0, 1.0, 0.0};
 					planes[n].mvec = spade::ctrs::cross_prod(planes[n].nvec, planes[n].tvec);
 				}
-				else if (slice_plane_in[n] == "file")
+				else if (slice_plane_in[n] == "3-points")
 				{
-					// Do something here
+					// Import 3-points and form plane vectors
 					slice_plane[n] = 3;
 					import_slice_points(n);
+				}
+				else if (slice_plane_in[n] == "file")
+				{
+					// Import probe locations
+					slice_plane[n] = 4;
+					import_probe_points(n);
 				}
 				else
 				{
@@ -155,6 +232,10 @@ namespace pptb::analysis
 
 				// Set plane point (planes specified by Cartesian directions)
 				if (slice_plane[n] < 3) planes[n].pt = get_plane_pt(n);
+
+				// Save truncation settings
+				truncate[n] = truncate_in[n];
+				truncate[n].val *= scaling;
 
 				// Write details to screen
 				write_slice_details(n, slice_plane_in);
@@ -191,7 +272,7 @@ namespace pptb::analysis
 
 		// Slice provided data set using specified plane vectors
 		template <typename geom_t>
-		surf_geom_t slice_by_plane(const int& islice, const int& tdir1, const int& tdir2, const geom_t& surface)
+		surf_geom_t slice_by_plane(const int& islice, const geom_t& surface)
 		{
 			// Output type
 			surf_geom_t slice;
@@ -201,25 +282,40 @@ namespace pptb::analysis
 			std::vector<std::size_t> elemIntersect; // List of intersecting elements
 			for (int n = 0; n<surface.connect.size(); n++)
 			{
-				// Node list for this element
-				const auto& list = surface.connect[n];
-					
-				// Sweep nodes
-				int count_pos = 0, count_neg = 0;
-				for (int inode = 0; inode<3; inode++)
+				// Centroid for this element
+				const auto& xyz  = surface.centroid(n);
+
+				// Do we have truncation settings on?
+				const bool clip  = truncate[islice].type > -1;
+
+				// Operator
+				bool check_elem = false;
+				if (truncate[islice].type == 0)      check_elem = xyz[truncate[islice].dir] > truncate[islice].val;
+				else if (truncate[islice].type == 1) check_elem = xyz[truncate[islice].dir] < truncate[islice].val;
+
+				// Do we clip?
+				if (clip && check_elem || !clip)
 				{
-					// Node coordinate
-					const auto& node = surface.nodes[list[inode]];
-						
-					// Distance from point to plane
-					const value_type dist = dist_to_plane(islice, node);
-						
-					if (dist>0) count_pos++;
-					if (dist<0) count_neg++;
-				}
+					// Node list for this element
+					const auto& list = surface.connect[n];
 					
-				// Did we intersect the plane?
-				if (count_pos<3 && count_neg<3)	elemIntersect.push_back(n);
+					// Sweep nodes
+					int count_pos = 0, count_neg = 0;
+					for (int inode = 0; inode<3; inode++)
+					{
+						// Node coordinate
+						const auto& node = surface.nodes[list[inode]];
+						
+						// Distance from point to plane
+						const value_type dist = dist_to_plane(islice, node);
+						
+						if (dist>0) count_pos++;
+						if (dist<0) count_neg++;
+					}
+					
+					// Did we intersect the plane?
+					if (count_pos<3 && count_neg<3)	elemIntersect.push_back(n);
+				}
 			}
 
 			// Select slice plane data storage
@@ -281,75 +377,63 @@ namespace pptb::analysis
 			return slice;
 		}
 
-		// Extract data using preset coordinates
+		// Slice provided data set using probe list
 		template <typename geom_t>
-		surf_geom_t slice_by_file(const int& islice, const geom_t& surface)
+		void slice_by_probes(const int& islice, surf_geom_t& slice, const geom_t& surface)
 		{
-			// Get slice file points
-			const auto& file_points = slice_file_points[islice];
-			
-			// Initialize slice operator
-			surf_geom_t slice;
-
 			// Select slice plane data storage
 			slice.maxNpe = 2; // Planar vtk
 
+			// Number of probes
+			const std::size_t npts = slice.nodes.size();
+
+			// Find closest element to each probe
+			std::vector<std::size_t> elemIntersect; // List of intersecting elements
+			for (int n = 0; n<surface.connect.size(); n++)
+			{
+				// Node list for this element
+				const auto& list = surface.connect[n];
+					
+				// Sweep nodes
+				int count_pos = 0, count_neg = 0;
+				for (int inode = 0; inode<3; inode++)
+				{
+					// Node coordinate
+					const auto& node = surface.nodes[list[inode]];
+						
+					// Distance from point to plane
+					const value_type dist = dist_to_plane(islice, node);
+						
+					if (dist>0) count_pos++;
+					if (dist<0) count_neg++;
+				}
+					
+				// Did we intersect the plane?
+				if (count_pos<3 && count_neg<3)	elemIntersect.push_back(n);
+			}
+
 			// Set cross-section memory
-			const int npts = slice_file_points[islice].size();
-			slice.nodes.resize(npts);
-			slice.connect.resize(npts);
+			slice.connect.resize(elemIntersect.size());
 			slice.varnames.resize(surface.varnames.size());
 			slice.varnames = surface.varnames;
 			slice.data.resize(surface.varnames.size());
 			for (int i = 0; i<slice.varnames.size(); i++)
 			{
 				auto& data_loc = slice.data[i];
-				data_loc.resize(npts);
+				data_loc.resize(elemIntersect.size());
 			}
 
-			// Going to brute force this for now
-			for (int ipt = 0; ipt<npts; ++ipt)
-			{
-				// Coordinate on slice plane
-				const auto& xyz = file_points[ipt];
-
-				// Store in slice struct
-				slice.nodes[ipt][0] = xyz[0];
-				slice.nodes[ipt][1] = xyz[1];
-				slice.nodes[ipt][2] = xyz[2];
-				
-				int minIdx = 0;
-				value_type minDist = 1e9;
-				for (int n = 0; n<surface.connect.size(); ++n) // Sweep centroids
-				{
-					if (surface.data[0][n] > 0)
-					{
-						// Element centroid
-						const auto centroid = surface.centroid(n);
-
-						// Compute distance
-						const value_type dist = spade::ctrs::array_norm(xyz, centroid);
-
-						if (dist < minDist)
-						{
-							minDist = dist;
-							minIdx  = n;
-						}
-					}
-				}
-
-				// Assign data from identified element onto slice plane
-				for (int n = 0; n<slice.varnames.size(); n++)
-				{
-					slice.data[n][ipt] = surface.data[n][minIdx];
-				}
-			}
-
-			// Get min/max L
+			// First, sweep points intersecting the plane and build the plane cross-section
 			value_type minL =   1e9;
 			value_type maxL = - 1e9;
-			for (int n = 0; n<npts; n++)
+			for (int n = 0; n<elemIntersect.size(); n++)
 			{
+				// Element ID
+				const std::size_t fglob = elemIntersect[n];
+
+				// Copy data onto slice plane
+				for (int ivar = 0; ivar<slice.varnames.size(); ivar++) slice.data[ivar][n] = surface.data[ivar][fglob];
+
 				// Find maximum coordinate extent in normalization direction
 				if (slice.nodes[n][normalize_dir]<minL) minL = slice.nodes[n][normalize_dir];
 				if (slice.nodes[n][normalize_dir]>maxL) maxL = slice.nodes[n][normalize_dir];
@@ -373,8 +457,6 @@ namespace pptb::analysis
 					slice.nodes[n][ntdir2]         = slice.nodes[n][ntdir2] / (maxL - minL);
 				}
 			}
-			
-			return slice;
 		}
 
 		// Function to extract data from specified geometry
@@ -396,9 +478,9 @@ namespace pptb::analysis
 				const auto& node2 = surface.nodes[connect[2]];
 
 				// Grid spacings
-				const auto dx0   = spade::ctrs::array_norm(node0, node1);
-				const auto dx1   = spade::ctrs::array_norm(node0, node2);
-				const auto dx2   = spade::ctrs::array_norm(node1, node2);
+				const auto dx0   = spade::ctrs::array_norm(node0 - node1);
+				const auto dx1   = spade::ctrs::array_norm(node0 - node2);
+				const auto dx2   = spade::ctrs::array_norm(node1 - node2);
 
 				// Average spacing
 				dx_avg += (dx0 + dx1 + dx2) / value_type(3.0 * surface.connect.size());
@@ -419,64 +501,8 @@ namespace pptb::analysis
 				const int tdir2 = std::abs(0*planes[islice].mvec[0] + 1*planes[islice].mvec[1] + 2*planes[islice].mvec[2]);
 				
 				auto& slice = slice_data[islice];
-				if (slice_plane[islice]<3)
-				{
-					slice = slice_by_plane(islice, tdir1, tdir2, surface);
-				}
-				else
-				{
-					// Slice by points in file
-					slice = slice_by_file(islice, surface);
-				}
-
-				// Sorting only works when aligned with Cartesian directions for now...
-//				if (slice_plane[islice] < 3)
-//				{
-//					// Get min/max L and geom centroid
-//					const int npts = slice.nodes.size();
-//					std::vector<value_type> geom_centroid;
-//					geom_centroid.resize(3, 0.0);
-//					for (int n = 0; n<npts; n++)
-//					{
-//						// Geometry midpoint
-//						geom_centroid[0] += slice.nodes[n][0] / value_type(npts);
-//						geom_centroid[1] += slice.nodes[n][1] / value_type(npts);
-//						geom_centroid[2] += slice.nodes[n][2] / value_type(npts);
-//					}
-//					
-//					// Assemble structure to be sorted
-//					std::vector<sliceElement_t<value_type>> sliceElements;
-//					sliceElements.resize(slice.nodes.size());
-//					for (int n = 0; n<slice.nodes.size(); n++)
-//					{
-//						sliceElements[n].xyz[0] = slice.nodes[n][0];
-//						sliceElements[n].xyz[1] = slice.nodes[n][1];
-//						sliceElements[n].xyz[2] = slice.nodes[n][2];
-//						for (int ivar = 0; ivar<slice.varnames.size(); ivar++)
-//							sliceElements[n].data[ivar] = slice.data[ivar][n];
-//					}
-//
-//					// Comparison function
-//					auto compare = [&](const sliceElement_t<value_type>& elemA, const sliceElement_t<value_type>& elemB)
-//					{
-//						const value_type phiA = atan2(elemA.xyz[tdir2] - geom_centroid[tdir2], elemA.xyz[tdir1] - geom_centroid[tdir1]);
-//						const value_type phiB = atan2(elemB.xyz[tdir2] - geom_centroid[tdir2], elemB.xyz[tdir1] - geom_centroid[tdir1]);
-//						return phiA > phiB;
-//					};
-//				
-//					// Sort points
-//					std::sort(sliceElements.begin(), sliceElements.end(), compare);
-//
-//					// Push data back into slice geom
-//					for (int n = 0; n<npts; n++)
-//					{
-//						slice.nodes[n][0] = sliceElements[n].xyz[0];
-//						slice.nodes[n][1] = sliceElements[n].xyz[1];
-//						slice.nodes[n][2] = sliceElements[n].xyz[2];
-//						for (int ivar = 0; ivar<slice.varnames.size(); ivar++)
-//							slice.data[ivar][n] = sliceElements[n].data[ivar];
-//					}
-//				}
+				if (slice_plane[islice]<4) slice = slice_by_plane(islice, surface);
+				else slice_by_probes(islice, slice, surface);
 				
 				// Set connectivity
 				for (int n = 0; n<slice.nodes.size(); n++)
@@ -486,7 +512,6 @@ namespace pptb::analysis
 					list[1] = n+1;
 					if (list[1]>slice.nodes.size()-1) list[1] = 0;
 				}
-				
 			} // End slice loop
 		} // End extraction function
 
@@ -547,6 +572,12 @@ namespace pptb::analysis
 
 			// Close file
 			fh.close();
+
+			for (int islice = 0; islice<nslice; islice++)
+			{
+				const std::string fname = slice_name[islice]+".vtk";
+				io::write_vtk_data<double>(fname, slice_data[islice]);
+			}
 			
 		} // End export
 	};
--- src/utils/utils.h
+++ src/utils/utils.h
@@ -35,4 +35,18 @@ namespace pptb::utils
 			print("Number of files found =  ",nFiles);
 		}
 	};
+
+	std::vector<std::string> splitString(const std::string& str, const std::string& delimiter)
+	{
+		std::vector<std::string> tokens;
+		size_t start = 0;
+		size_t end = str.find(delimiter);
+		while (end != std::string::npos) {
+			tokens.push_back(str.substr(start, end - start));
+			start = end + delimiter.length();
+			end = str.find(delimiter, start);
+		}
+		tokens.push_back(str.substr(start));
+		return tokens;
+	}
 }
